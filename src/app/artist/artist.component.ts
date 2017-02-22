55ebf0b57025d98b7e2e59d33125f2e10485a0a7
Michael Schennum <mikeschenphoto@gmail.com>
2017-02-22 14:24:51 -0700

    adds fav artist from spotify search

--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Spotify } from '../spotify';
 import {Artist} from '../../Artist';
+import { Album } from '../album.model';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
   styleUrls: ['./artist.component.scss'],
-  providers: [Spotify]
+  providers: [Spotify, AlbumService]
 })
 export class ArtistComponent {
 	 searchRes: Artist[];
 
-  constructor(private spotify: Spotify) { }
+  constructor(private spotify: Spotify, private albumService: AlbumService) { }
 
   searchArtist(url: string) {
   	this.spotify.searchArtist(url)
   	 .subscribe(res => {
      		this.searchRes = res.artists.items;
      })
+  }
+
+  submitArtist(title: string, artist: string, description: string) {
+    var newAlbum: Album = new Album(title, artist, description);
+    console.log(newAlbum);
+    this.albumService.addAlbum(newAlbum);
   }
 }
